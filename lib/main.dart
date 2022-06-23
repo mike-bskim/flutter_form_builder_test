44d97d77d557268ccb041bb126e7ba53af90bf79
mike.bskim <mike.bskim@gmail.com>
2022-06-23 20:22:17 +0200

    04_dropdown&TypeAheadFormField&FormBuilderTextField

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:intl/intl.dart';
 
@@ -33,6 +34,7 @@ class MyHomePageState extends State<MyHomePage> {
   final GlobalKey<FormBuilderState> _fbKey = GlobalKey<FormBuilderState>();
   final DateTime startDate = DateTime.now();
   AutovalidateMode autoValidate = AutovalidateMode.disabled;
+  final TextEditingController _typeAheadController = TextEditingController();
 
   void _submit() {
     setState(() {
@@ -48,9 +50,10 @@ class MyHomePageState extends State<MyHomePage> {
     form.save();
 
     // final inputValues = _fbKey.currentState!.value;
-    final inputValues = form.value;
+    var inputValues = form.value;
     debugPrint(inputValues.toString());
-    //
+    debugPrint(_typeAheadController.text.toString());
+
     // Navigator.pushReplacement(
     //   context,
     //   MaterialPageRoute(builder: (context) {
@@ -60,33 +63,48 @@ class MyHomePageState extends State<MyHomePage> {
     //
   }
 
-  // List<String> getSuggestion(String query) {
-  //   if (query.isEmpty) {
-  //     return [];
-  //   }
-  //
-  //   List<String> matches = [];
-  //   final regionNames = regions.map((region) {
-  //     return region['regionName'];
-  //   }).toList();
-  //
-  //   matches.addAll(regionNames);
-  //
-  //   matches.retainWhere((s) => s.contains(query));
-  //   return matches;
-  // }
-  //
-  // bool _checkRegionName(String regionName) {
-  //   final foundRegion = regions.firstWhere((region) {
-  //     return region['regionName'] == regionName;
-  //   }, orElse: () => null);
-  //
-  //   return foundRegion == null ? false : true;
-  // }
+// 입력한 단어가 포함한 모든 리스트 리턴
+  List<String> getSuggestion(String query) {
+    if (query.isEmpty) {
+      return [];
+    }
 
-  InputDecoration inputDecoration(String label) => InputDecoration(
+    List<String> matches = [];
+    List<String> regionNames = regions.map((region) {
+      return region['regionName']!;
+    }).toList();
+
+    // for(var regionName in regionNames) {
+    //   matches.add(regionName);
+    // }
+    matches.addAll(regionNames);
+
+    matches.retainWhere((s) => s.contains(query));
+    return matches;
+  }
+
+// 입력한 단어가 리스트에 존재하는지 확인.
+  bool _checkRegionName(String regionName) {
+    final foundRegion = regions.firstWhere(
+      (region) {
+        // 리턴 타입이 map 타입임
+        return region['regionName'] == regionName;
+      },
+      orElse: () {
+        // 정상인 리턴 타입이 map 타입이므로 여기도 map 타입으로 리턴해야 함
+        return {"_id": "null", "dongcode": "null", "regionName": "null"};
+      },
+    );
+
+    return foundRegion['regionName'] == "null" ? false : true;
+  }
+
+// InputDecoration 축약 스타일로
+  InputDecoration inputDecoration({required String label, String hint = ''}) =>
+      InputDecoration(
         filled: true,
         labelText: label,
+        hintText: hint,
         border: const OutlineInputBorder(),
       );
 
@@ -121,8 +139,9 @@ class MyHomePageState extends State<MyHomePage> {
                       lastDate: DateTime(
                           startDate.year + 1, startDate.month, startDate.day),
                       format: DateFormat('yyyy-MM-dd'),
-                      decoration: inputDecoration('시작일'),
+                      decoration: inputDecoration(label: '시작일'),
                       // 하나만 검증하려면 required 사용
+                      // 여러가지 검증하려면 compose 사용
                       validator: FormBuilderValidators.required(
                         errorText: '시작일은 필수입니다',
                       ),
@@ -141,15 +160,16 @@ class MyHomePageState extends State<MyHomePage> {
                       lastDate: DateTime(
                           startDate.year + 1, startDate.month, startDate.day),
                       format: DateFormat('yyyy-MM-dd'),
-                      decoration: inputDecoration('종료일'),
+                      decoration: inputDecoration(label: '종료일'),
+                      // 하나만 검증하려면 required 사용
                       // 여러가지 검증하려면 compose 사용
                       validator: FormBuilderValidators.compose([
                         FormBuilderValidators.required(errorText: '종료일은 필수입니다'),
                         (val) {
-                          debugPrint('val: $val');
+                          // debugPrint('val: $val');
                           final sd =
                               _fbKey.currentState!.fields['startDate']!.value;
-                          debugPrint('sd: $sd');
+                          // debugPrint('sd: $sd');
 
                           if (sd != null && sd.isAfter(val)) {
                             return '시작일이 종료일보다 뒤입니다';
@@ -158,26 +178,61 @@ class MyHomePageState extends State<MyHomePage> {
                         },
                       ]),
                     ),
-                    // const SizedBox(height: 20),
-                    // FormBuilderDropdown(
-                    //   attribute: 'cropId',
-                    //   items: crops.map<DropdownMenuItem<String>>((crop) {
-                    //     return DropdownMenuItem<String>(
-                    //         value: crop['id'], child: Text(crop['cropName']));
-                    //   }).toList(),
-                    //   hint: const Text('대상 품종을 선택하세요'),
-                    //   decoration: const InputDecoration(
-                    //     filled: true,
-                    //     labelText: '품종',
-                    //     border: OutlineInputBorder(),
-                    //   ),
-                    //   validators: [
-                    //     FormBuilderValidators.required(
-                    //       errorText: '품종을 필수입니다',
-                    //     ),
-                    //   ],
-                    // ),
-                    // const SizedBox(height: 20),
+                    const SizedBox(height: 20),
+                    FormBuilderDropdown(
+                      name: 'cropId',
+                      items: crops.map<DropdownMenuItem<String>>((crop) {
+                        return DropdownMenuItem<String>(
+                            value: crop['id'], child: Text(crop['cropName']!));
+                      }).toList(),
+                      hint: const Text('대상 품종을 선택하세요'),
+                      decoration: inputDecoration(label: '품종'),
+                      validator: FormBuilderValidators.compose([
+                        FormBuilderValidators.required(errorText: '품종을 필수입니다'),
+                      ]),
+                    ),
+                    Visibility(
+                      visible: false,
+                      child: FormBuilderTextField(
+                        controller: _typeAheadController,
+                        name: 'regionName',
+                        decoration: const InputDecoration(
+                          labelText:
+                              'This value is passed along to the [Text.maxLines] attribute of the [Text] widget used to display the hint text.',
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    TypeAheadFormField(
+                      textFieldConfiguration: TextFieldConfiguration(
+                        controller: _typeAheadController,
+                        decoration: inputDecoration(
+                            label: '시군구', hint: '시군구 자동으로 입력됩니다'),
+                      ),
+                      suggestionsCallback: (String pattern) {
+                        return getSuggestion(pattern);
+                      },
+                      itemBuilder: (context, suggestion) {
+                        return ListTile(
+                          title: Text(suggestion.toString()),
+                        );
+                      },
+                      onSuggestionSelected: (suggestion) {
+                        debugPrint(suggestion.toString());
+                        _typeAheadController.text = suggestion.toString();
+                        // _checkRegionName1(_typeAheadController.text);
+                      },
+                      // 미입력 검증, 입력하다가 중단한 경우 검증
+                      validator: FormBuilderValidators.compose([
+                        FormBuilderValidators.required(errorText: '시군구는 필수입니다'),
+                        (val) {
+                          if (!_checkRegionName(val!)) {
+                            return '잘못된 지역 이름입니다';
+                          }
+                          return null;
+                        }
+                      ]),
+                    ),
                     // FormBuilderTypeAhead(
                     //   attribute: 'regionName',
                     //   decoration: const InputDecoration(
@@ -206,27 +261,24 @@ class MyHomePageState extends State<MyHomePage> {
                     //     );
                     //   },
                     // ),
-                    // const SizedBox(height: 20),
-                    // FormBuilderTextField(
-                    //   attribute: 'area',
-                    //   decoration: const InputDecoration(
-                    //     filled: true,
-                    //     labelText: '면적',
-                    //     hintText: '제곱미터 단위로 면적을 입력하세요',
-                    //     border: OutlineInputBorder(),
-                    //   ),
-                    //   validators: [
-                    //     FormBuilderValidators.required(errorText: '면적은 필수입니다'),
-                    //     FormBuilderValidators.numeric(errorText: '숫자만 입력하세요'),
-                    //     (val) {
-                    //       final area = double.parse(val);
-                    //       if (area < 100 || area > 10000) {
-                    //         return '유효면적은 100에서 10000 사이입니다';
-                    //       }
-                    //       return null;
-                    //     }
-                    //   ],
-                    // ),
+                    const SizedBox(height: 20),
+                    FormBuilderTextField(
+                      name: 'area',
+                      decoration: inputDecoration(
+                          label: '면적', hint: '제곱미터 단위로 면적을 입력하세요'),
+                      // 미입력 검증, 숫자만 입력가능, 범위이내 검증
+                      validator: FormBuilderValidators.compose([
+                        FormBuilderValidators.required(errorText: '면적은 필수입니다'),
+                        FormBuilderValidators.numeric(errorText: '숫자만 입력하세요'),
+                        (val) {
+                          final area = double.parse(val!);
+                          if (area < 100 || area > 10000) {
+                            return '유효면적은 100에서 10000 사이입니다';
+                          }
+                          return null;
+                        }
+                      ]),
+                    ),
                     // const SizedBox(height: 20),
                     // FormBuilderRadio(
                     //   attribute: 'urgent',
